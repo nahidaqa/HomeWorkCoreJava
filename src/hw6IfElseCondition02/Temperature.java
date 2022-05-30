3ec769c8abd7c4c858caa74e10300c360f5d500c
nahidaqa <nahidaislamdev@gmail.com>
2022-05-30 01:52:56 -0400

    Homework 07 add

--- src/hw6IfElseCondition02/Temperature.java
+++ src/hw6IfElseCondition02/Temperature.java
@@ -1,5 +1,9 @@
 package hw6IfElseCondition02;
 
+/*Create a package name --> "hw6IfElseCondition02" inside your HW project. Create a Class, name "Temperature". Type the main method.  After the main method, Use Scanner class to define a int type variable todaysTemperature. Now write some condition. a) If temperature is less than 32, outcome will be "Freezing" b)  If temperature is less than 55, outcome will be "Pleasant"  c) If temperature is less than 73, outcome will be "Getting Warmer"   d)  If temperature is less than 101, outcome will be "Hot" And finally e) the outcome will be "Ice on my Head Please". In the above 5 condition, use appropriate key word like if, else if, else to execute today's Temperature. Run the code and type today's temperature in console and find the Temperature of Today. Please organize the code (very important). don't give any white space. push the code to GitHub and add the link below.
+ 
+ */
+
 import java.util.Scanner;
 
 public class Temperature {
